dae11358afbe4c7337b65af8c7da5ebf30f6d6f7
avinash <avinashpanneer@gmail.com>
2024-05-23 23:48:58 +0530

    caching and UI works but caches for uri path and not args

--- weather/app.py
+++ weather/app.py
@@ -1,19 +1,36 @@
 from flask import Flask, request
+from flask_caching import Cache
 from flask_cors import CORS
 import requests
 from requests import HTTPError
 from datetime import datetime
+import os
+import redis
 
 app = Flask(__name__)
+
+app.config['CACHE_TYPE'] = 'redis'
+app.config['CACHE_REDIS_HOST'] = 'localhost'
+app.config['CACHE_REDIS_PORT'] = 6379
+app.config['CACHE_REDIS_DB'] = 0
+
+cache = Cache(app)
+cache.init_app(app)
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
 CORS(app)
 
-API_KEY = "F4RZ8KWXHMFARTPNDRBR8M97Z"
+API_KEY = os.environ.get('API_KEY')
 
 @app.get("/weather")
+@cache.cached(timeout=3600)
 def get_weather():
+
     pincode = request.args.get('pincode')
     todays_date = datetime.today().strftime('%Y-%m-%d')
     endpoint = lambda pincode, date1: f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{pincode}/{date1}?key={API_KEY}"
+
     try:
         outbound_response = requests.get(endpoint(pincode, todays_date))
         outbound_response.raise_for_status()
