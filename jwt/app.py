df92fb1e92aec7282df2a581b74e24be501d7571
avinash <avinashpanneer@gmail.com>
2024-06-10 22:29:31 +0530

    login now returns jwt. can get authorised content by sending jwt in header

--- jwt/app.py
+++ jwt/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from bcrypt import checkpw, hashpw, gensalt
+import jwt
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
@@ -22,6 +24,7 @@ class User(db.Model):
         self.hashed_password = hashpw(password, gensalt())
 
     def check_password(self, password):
+        print(password, self.hashed_password)
         return checkpw(password, self.hashed_password)
 
 with app.app_context():
@@ -45,30 +48,48 @@ def register_new_user():
     db.session.commit()
     return {"success": True, "message": "registered successfully"}, 201 # create success
 
-@app.post("/login")
+@app.post("/auth")
 def login():
     payload = request.get_json()
     username = payload.get('username')
     password = payload.get('password')
 
-    print(password, hash(password))
-
     user = db.session.query(User).filter(User.username == username).scalar()
-
-    print(user.hashed_password)
 
     if user is None:
         return {"success": False, "message": "user doesn't exist"}, 404 # not found
     else:
         is_correct_password = user.check_password(password)
         if is_correct_password:
-            return {"success": True, "message": "login succesfull"}, 200 # general success
+            new_generated_token = jwt.encode({"username": username}, key=os.environ['SECRET'], algorithm='HS256')
+            return {"success": True, "message": "login succesfull", "token": new_generated_token}, 200 # general success
         else:
             return {"success": False, "message": "incorrect password"}, 401 # unauthorised
         
+@app.get("/authorised_content")
+def get_authorised_content():
+        auth_header = request.headers.get('Authorization')
+        token = auth_header.split(' ')[1]
+        try:
+            payload = jwt.decode(
+                token,
+                key=os.environ['SECRET'],
+                algorithms=['HS256']
+            )
+            username = payload["username"]
+            response = {"success": True, "message": f"Congrats {username}. You may enter."}, 200
+        except jwt.exceptions.InvalidTokenError as E:
+            print('invalid token exception', E)
+            response = {"success": False, "message": "You don't belong here."}, 401
+        except Exception as E:
+            print('generic exception', E)
+            response = {"success": False, "message": "You don't belong here."}, 401
+        print('auth content', response)
+        return response
+
 @app.get("/test")
 def test():
-    return "hi"
+    return "hi", 200
 
 
 
